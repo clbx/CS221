213f8164db8a2ed4ad52389717a5c51fa3255bba
Clay Buxton <clay@clbx.io>
2018-10-02 14:41:13 -0500

    Finished Project

--- Converter&Calculator/src/ConvertCalculate.java
+++ Converter&Calculator/src/ConvertCalculate.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
  */
 public class ConvertCalculate {
 
-  public static void main(String[] args) throws StackException {
+  public static void main(String[] args) throws StackException{
     Scanner in = new Scanner(System.in);
     System.out.println("Enter your infix equation");
     String infix = in.nextLine();
     Postfix postfix = new Postfix(infix);
     postfix.processInfix();
     System.out.println("Result: " + postfix.calculatePostfix());
-  }
 
+
+
+  }
 }
+
