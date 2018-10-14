2f9c0d48c3ec80d8d12612153af0243075e2279f
Clay Buxton <clay@clbx.io>
2018-10-14 18:08:54 -0400

    Added Documentation and Tests

--- QuickSort/src/QuickSort.java
+++ QuickSort/src/QuickSort.java
@@ -1,23 +1,75 @@
-import java.lang.reflect.Array;
+/**
+ * @author buxtonc
+ *
+ */
 
 public class QuickSort {
 
-  public static void main(String[] args) {
-    int total = 0;
+  //Divisor for
+  private final static int nanoConv = 1000000000;
 
-    for(int i = 0; i < 5; i++) {
-      int arr[] = new int[10000000];
+  /**
+   * main
+   */
+  public static void main(String[] args) {
+    int runs = 100;          //The number of runs to run
+    int arrLen = 12500000;   //The number of elements in the arrays
+    double runTimes[] = new double[runs];
+
+    System.out.println("[ Quick Sort Runs ]");
+    System.out.printf ("[n = %d] [Runs: %d]\n",arrLen,runs);
+    System.out.println("-------------------");
+
+    for (int i = 0; i < runs; i++) {
+      int arr[] = new int[arrLen];
       genArray(arr);
       double startTime = System.nanoTime();
       sort(arr);
       double endTime = System.nanoTime();
       double duration = (endTime - startTime);
-      System.out.printf("Array [n = %d] Sorted - Time: %.3fs\n", arr.length,duration / 1000000000);
+      runTimes[i] = duration;
+      System.out.printf("Run [%d] Array Sorted - Time: %.3fs\n",i+1,duration / nanoConv);
     }
 
+    printStatistics(runTimes);
   }
 
-  static int part(int arr[], int low, int high){
+
+  /**
+   * Prints some statistics about the runs
+   * @param runTimes the array with run times.
+   */
+  private static void printStatistics(double[] runTimes){
+     //Get Min & Max
+    double min = Double.MAX_VALUE;
+    double max = 0;
+    for(double i : runTimes){
+      if(i < min)
+        min = i;
+      if(i > max)
+        max = i;
+    }
+
+    //Get Average
+    double avg = 0;
+    for(double i : runTimes)
+      avg += i;
+    avg /= runTimes.length;
+    System.out.println("-------------------");
+    System.out.printf("Max Run: %.3fs ", max/nanoConv);
+    System.out.printf("Min Run: %.3fs ", min/nanoConv);
+    System.out.printf("Avg Run: %.3fs ", avg/nanoConv);
+  }
+
+  /**
+   * This selects the pivot as high
+   *
+   * @param arr the array that will be parted
+   * @param low the low limit
+   * @param high the high limit
+   * @return the new position of the pivot
+   */
+  private static int part(int arr[], int low, int high){
 
     int pivot = arr[high];
 
@@ -41,29 +93,55 @@ public class QuickSort {
     return i+1;
   }
 
-  static void sort(int arr[]){
+  /**
+   * An overload class for the sort, allowing to just give an array instead of all the parameters.
+   * @param arr the array to be sorted
+   */
+  private static void sort(int arr[]){
     sort(arr, 0, arr.length-1);
   }
 
-  static void sort(int arr[], int low, int high){
+  /**
+   *
+   * @param arr The array to be sorted
+   * @param low The low limit
+   * @param high The high limit
+   */
+  private static void sort(int arr[], int low, int high){
     if(low < high){
       int index = part(arr, low, high);
-
 
       sort(arr, low, index-1);
       sort(arr, index+1, high);
     }
   }
 
+  /**
+   * Prints the array
+   * @param arr the array to be printed
+   */
   static void printArray(int arr[]){
     for(int i : arr)
       System.out.print(i + " ");
   }
 
-
-  static void genArray(int arr[]){
+  /**
+   * Generates an random array
+   * @param arr the array to fill up
+   */
+  private static void genArray(int arr[]){
     for(int i = 0; i < arr.length-1; i++)
       arr[i] = (int)(Math.random() * (99999));
+  }
+
+  /**
+   * Runs one of the worst case scenarios for the quicksort.
+   * @param arr the array to be filled.
+   */
+  private static void worstArray(int arr[]){
+    for(int i = 0; i < arr.length-1; i++){
+      arr[i] = 5;
+    }
   }
 
 
